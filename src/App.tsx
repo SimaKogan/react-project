3d68d84db38d4ed93f00840fb4383ed672136789
SimaKogan <simas96@mail.ru>
2023-02-09 03:03:24 +0200

    React-Router Home-Orders-clients-Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css'
-import { CounterMultiply } from './components/CounterMultiply';
-import { CounterSquare } from './components/CounterSquare';
-import { CounterUpdater } from './components/CounterUpdater';
-import { Login } from './components/Login';
-import { Logout } from './components/Logout';
-import {useSelector} from 'react-redux';
-import { Input } from './components/Input';
+import { Layout } from './components/navigators/Layout';
+import { Products } from './components/navigators/Products';
+import { BreadProducts } from './components/pages/BreadProducts';
+import { Customers } from './components/pages/Customers';
+import { DairyProducts } from './components/pages/DairyProducts';
+import { Home } from './components/pages/Home';
+import { Orders } from './components/pages/Orders';
+
 function App() {
-  const auth: string = useSelector<any, string>(state => state.auth.authenticated);
-  const [operand, setOperand] = React.useState(1);
-  const [factor, setFactor] = React.useState(10);
-  return <div>
-    {auth && <div>
-      <p>Username is {auth}</p>
-        { auth.includes("admin") && <Input placeHolder={'Enter operand'} inputProcess={function (value: string):
-         string {
-        setOperand(+value);
-        return '';
-      } }></Input>}
-        <Input placeHolder={'Enter factor'} inputProcess={function (value: string):
-         string {
-        setFactor(+value);
-        return '';
-      } }></Input>
-      </div>}
-    <div>
-    <CounterUpdater operand={operand}></CounterUpdater>
-    <CounterSquare></CounterSquare>
-    <CounterMultiply factor={factor}></CounterMultiply>
-  </div>
-  {auth && <Logout></Logout>}
-  {!auth && <Login></Login>}
-  </div>
+  return <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Layout/>}>
+              <Route index element={<Home/>}></Route>
+              <Route path='customers' element={<Customers/>}/>
+              <Route path='orders' element={<Orders/>}></Route>
+              <Route path='products' element={<Products/>}>
+                    <Route path='dairy' element={<DairyProducts/>}/>
+                    <Route path='bread' element={<BreadProducts/>}/>
+              </Route>
+          </Route>
+              
+      </Routes>
+  </BrowserRouter>
 
 }
 export default App;

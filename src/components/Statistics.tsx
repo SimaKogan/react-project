88761b912e37c214e372438cc8c30803b56ba79c
SimaKogan <simas96@mail.ru>
2023-03-09 01:05:27 +0200

    MobileNavigator

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -44,8 +44,7 @@ export const Statistics: React.FC<Props> = ({ title, field, objects }) => {
     }
 
 
-    return <Box sx={{ width: {xs:"95vw", sm:"70vw",md:"60vw"},
-     height: {xs: "50vh", md: "40vh"} }}>
+    return <Box sx={{ width: "50vw", height: "30Vh" }}>
         <Typography sx={{fontSize: "1.8em",
          fontWeight: "bold", textAlign: "center"}}>{title}</Typography>
         <DataGrid columns={columns} rows={[statistics]} />
--- src/components/forms/LoginForm.tsx
+++
@@ -1,3 +0,0 @@
-export const LoginForm: React.FC = () => {
-    return <div></div>
-}
